d26f512e4ba31a99a1864a24b0f4b537681f14bd
Raimundo Henriques <raimundo.henriques@gmail.com>
2022-08-05 18:02:41 +0100

    TA prep: created seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Article.destroy_all
+
+10.times do
+  title = Faker::Hacker.say_something_smart
+  content = Faker::Lorem.paragraph * 5
+  article = Article.new(title: title, content: content)
+  article.save
+end
